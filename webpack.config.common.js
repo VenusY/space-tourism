b4e3df23c19c7077a565d78e4606e95105c2dc70
Venus <venusyip@outlook.com>
2025-01-15 06:31:15 +0000

    Remove unnecessary lines in Webpack config files

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { type } = require('os');
 
 module.exports = {
   entry: './src/index.jsx',
